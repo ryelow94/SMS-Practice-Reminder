46dfad9ee30119128a3dc2877907ba75c24ed6ef
Ryan Logan <ryan_plogan@yahoo.com>
2021-09-29 17:32:21 -0700

    routes

--- sendSms.js
+++ sendSms.js
@@ -5,7 +5,7 @@ const client = require("twilio")(accountSid, authToken);
 function initialSms(userNumber) {
   client.messages
     .create({
-      body: 'Anyway, Ill be outside at midnight on your porch. let me know if theres anyone willing to face me',
+      body: 'Well i gues we will find out who the real man is at midnight',
       from: "+12817646556",
       to: userNumber, //phone
     })
