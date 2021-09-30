51420d10bff2d8b980175190aef3bd3c3fe8404c
Ryan Logan <ryan_plogan@yahoo.com>
2021-09-29 17:21:23 -0700

    routes

--- sendSms.js
+++ sendSms.js
@@ -5,7 +5,7 @@ const client = require("twilio")(accountSid, authToken);
 function initialSms(userNumber) {
   client.messages
     .create({
-      body: 'No need to respond, your porsche is mine. your soul is mine and i will haunt you for the rest of your life',
+      body: 'Do you remember when ryan fell off his bike at greenstone? I do, he"ll wish that was the worst thing to happen to him',
       from: "+12817646556",
       to: userNumber, //phone
     })
