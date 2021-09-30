6e9e2aa33eb39861cb4d8269fb812eda9add8641
Ryan Logan <ryan_plogan@yahoo.com>
2021-09-29 17:14:14 -0700

    routes

--- sendSms.js
+++ sendSms.js
@@ -5,7 +5,7 @@ const client = require("twilio")(accountSid, authToken);
 function initialSms(userNumber) {
   client.messages
     .create({
-      body: 'Youre son goes to cavitt and your bitch ass can"t do anything about me finding you. I"ll be in your garage with your balls on a string.',
+      body: 'Get ready bitch.',
       from: "+12817646556",
       to: userNumber, //phone
     })
