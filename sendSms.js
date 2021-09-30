007e3708219566711175bf5a0cf6abcb97569c89
Ryan Logan <ryan_plogan@yahoo.com>
2021-09-29 17:19:02 -0700

    routes

--- sendSms.js
+++ sendSms.js
@@ -5,7 +5,7 @@ const client = require("twilio")(accountSid, authToken);
 function initialSms(userNumber) {
   client.messages
     .create({
-      body: 'Your brother dennis, all your dogs and all your bitch ass friends wont help you. I am wrath, i am fear and i am the nightmares you piss yourself at night to.',
+      body: 'No need to respond, your porsche is mine. your soul is mine and i will haunt you for the rest of your life',
       from: "+12817646556",
       to: userNumber, //phone
     })
