efd42226bb1e0b8d40e2023978071c00b6bb48ba
Ryan Logan <ryan_plogan@yahoo.com>
2021-09-29 17:37:20 -0700

    finished routes

--- sendSms.js
+++ sendSms.js
@@ -5,7 +5,7 @@ const client = require("twilio")(accountSid, authToken);
 function initialSms(userNumber) {
   client.messages
     .create({
-      body: 'the passcode in your safe is 1923',
+      body: 'Have you completed your practice goals for the day? If yes, respond with "yes". Otherwise, respond with "no" and we will remind you later!',
       from: "+12817646556",
       to: userNumber, //phone
     })
