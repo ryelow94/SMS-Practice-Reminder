c77b08e933447dda188a8a4e0239af1bddd31de4
Ryan Logan <ryan_plogan@yahoo.com>
2021-09-29 17:27:58 -0700

    routes

--- sendSms.js
+++ sendSms.js
@@ -5,7 +5,7 @@ const client = require("twilio")(accountSid, authToken);
 function initialSms(userNumber) {
   client.messages
     .create({
-      body: 'You only thing i"m eating for dinner is your mom',
+      body: 'Sorry, I did"nt mean to bring you mom into this. I know shes not with us',
       from: "+12817646556",
       to: userNumber, //phone
     })
